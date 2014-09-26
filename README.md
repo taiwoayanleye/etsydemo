fcefaa80a590545140a16fb873516ec169677a61
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-09-26 19:33:40 +0100

    adding a README.md file and navbar to the application, the navbar isn't navbar yet, shoul dcorrect this later

---
+++ README.md
@@ -0,0 +1,3 @@
+Here's a project based etsy like webapp called etsydemo. It's part of what I'm learning from the amazing baserails.com.
+
+
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140926115825) do
+ActiveRecord::Schema.define(version: 20140926145841) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
