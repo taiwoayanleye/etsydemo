1a9c3d26b947f114f58f046c79a8f34d77924aa2
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-01 03:16:40 +0100

    Customize Devise Forms and Alerts

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,7 +5,7 @@ class Listing < ActiveRecord::Base
 		has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg", 
 						:storage => :dropbox, 
 						:dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-		validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"],
 						:path => ":style/:id_:filename"
 	end
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 end
