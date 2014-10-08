b2f4f253606e084e917300a123508abf48f3fd9a
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-10-08 17:35:00 +0100

    Charge credit cards

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
+  before_filter :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_filter :check_user, only: [:edit, :update, :destroy]
 
   def seller
@@ -33,12 +33,26 @@ class ListingsController < ApplicationController
     @listing = Listing.new(listing_params)
     @listing.user_id = current_user.id
 
+    if current_user.recipient.blank?
+      Stripe.api_key = ENV["STRIPE_API_KEY"]
+      token = params[:stripeToken]
+
+      recipient = Stripe::Recipient.create(
+        :name => current_user.name,
+        :type => "individual",
+        :bank_account => token
+        )
+
+      current_user.recipient = recipient.id
+      current_user.save
+    end
+
     respond_to do |format|
       if @listing.save
         format.html { redirect_to @listing, notice: 'Listing was successfully created.' }
-        format.json { render :show, status: :created, location: @listing }
+        format.json { render action: 'show', status: :created, location: @listing }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -50,9 +64,9 @@ class ListingsController < ApplicationController
     respond_to do |format|
       if @listing.update(listing_params)
         format.html { redirect_to @listing, notice: 'Listing was successfully updated.' }
-        format.json { render :show, status: :ok, location: @listing }
+        format.json { head :no_content }
       else
-        format.html { render :edit }
+        format.html { render action: 'edit' }
         format.json { render json: @listing.errors, status: :unprocessable_entity }
       end
     end
@@ -63,7 +77,7 @@ class ListingsController < ApplicationController
   def destroy
     @listing.destroy
     respond_to do |format|
-      format.html { redirect_to listings_url, notice: 'Listing was successfully destroyed.' }
+      format.html { redirect_to listings_url }
       format.json { head :no_content }
     end
   end
@@ -81,7 +95,7 @@ class ListingsController < ApplicationController
 
     def check_user
       if current_user != @listing.user
-        redirect_to root_url, alert: "Sorry this listing belongs to someone else"
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else"
       end
     end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,15 +2,13 @@ class OrdersController < ApplicationController
   before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
+  def sales
+    @orders = Order.all.where(seller: current_user).order("created_at DESC")
+  end
 
-
-def sales
-  @orders = Order.all.where(seller: current_user).order("created_at DESC")
-end
-
-def purchases
-  @orders = Order.all.where(buyer: current_user).order("created_at DESC")
-end
+  def purchases
+    @orders = Order.all.where(buyer: current_user).order("created_at DESC")
+  end
 
   # GET /orders/new
   def new
@@ -26,16 +24,34 @@ end
     @seller = @listing.user
 
     @order.listing_id = @listing.id
-    @order.buyer_id = current_user.id 
+    @order.buyer_id = current_user.id
     @order.seller_id = @seller.id
 
+    Stripe.api_key = ENV["STRIPE_API_KEY"]
+    token = params[:stripeToken]
+
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@listing.price * 100).floor,
+        :currency => "usd",
+        :card => token
+        )
+    rescue Stripe::CardError => e
+      flash[:danger] = e.message
+    end
+
+    # transfer = Stripe::Transfer.create(
+    #   :amount => (@listing.price * 95).floor,
+    #   :currency => "usd",
+    #   :recipient => @seller.recipient
+    #   )
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to root_url, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
+        format.html { redirect_to root_url, notice: "Thanks for ordering!" }
+        format.json { render action: 'show', status: :created, location: @order }
       else
-        format.html { render :new }
+        format.html { render action: 'new' }
         format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
