e9e04f77f31a96178350b92cd2e669c0591d0dfa
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-11-03 04:38:09 +0000

    Add provider to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008182522) do
+ActiveRecord::Schema.define(version: 20141103043712) do
 
   create_table "listings", force: true do |t|
     t.string   "name"
@@ -52,6 +52,8 @@ ActiveRecord::Schema.define(version: 20141008182522) do
     t.datetime "updated_at"
     t.string   "name"
     t.string   "recipient"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
