17e1bd995dc316b80c7626fc92f9ec88d5fc82a6
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-11-03 11:41:56 +0000

    Resolve twitter authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,29 +14,10 @@ class User < ActiveRecord::Base
 		end
 	end
 
-	# def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
- #    user = User.where(:provider => auth.provider, :uid => auth.uid).first
- #    if user
- #      return user
- #    else
- #      registered_user = User.where(:email => auth.uid + "@twitter.com").first
- #      if registered_user
- #        return registered_user
- #      else
+    #VALIDATION 
+	validates :name, :email, :password, presence: true
 
- #        user = User.create(name:auth.extra.raw_info.name,
- #                            provider:auth.provider,
- #                            uid:auth.uid,
- #                            email:auth.uid+"@twitter.com",
- #                            password:Devise.friendly_token[0,20],
- #                          )
- #    end
- #  	end
- #  	end
-     
-validates :name, :email, :password, presence: true
-
-has_many :listings, dependent: :destroy
-has_many :sales, class_name: "Order", foreign_key: "seller_id"
-has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
+	has_many :listings, dependent: :destroy
+	has_many :sales, class_name: "Order", foreign_key: "seller_id"
+	has_many :purchases, class_name: "Order", foreign_key: "buyer_id"
 end
