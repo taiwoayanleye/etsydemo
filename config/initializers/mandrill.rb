a0b2f92f76472efad3164938b492021ceabc993e
Taiwo Ayanleye <onlyyouandty@yahoo.com>
2014-11-07 19:42:38 +0000

    Set welcom_letter

--- config/initializers/mandrill.rb
+++ config/initializers/mandrill.rb
@@ -1,4 +1,4 @@
 require 'mandrill'
 m = Mandrill::API.new # All official Mandrill API clients will automatically pull your API key from the environment
-rendered = m.templates.render 'MyTemplate', [{:name => 'main', :content => 'The main content block'}]
+rendered = m.templates.render 'welcome_letter', [{:name => 'main', :content => 'The main content block'}]
 puts rendered['html'] # print out the rendered HTML
